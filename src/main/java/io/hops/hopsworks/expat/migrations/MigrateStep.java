04d8bed5cf8f0cf8d4a8fe45178452216a693dbc
Fabio Buso <buso.fabio@gmail.com>
2018-10-30 18:17:55 +0100

    Add copyright header

--- src/main/java/io/hops/hopsworks/expat/migrations/MigrateStep.java
+++ src/main/java/io/hops/hopsworks/expat/migrations/MigrateStep.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Expat
+ * Copyright (C) 2018, Logical Clocks AB. All rights reserved
+ *
+ * Expat is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Expat is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package io.hops.hopsworks.expat.migrations;
 
 public interface MigrateStep {
