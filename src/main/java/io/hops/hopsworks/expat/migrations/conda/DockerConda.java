c1ca141eb23b1e925d18275b9cd38c59762fa076
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-08-10 09:59:15 +0200

    Temporarily disable DockerConda expat

--- src/main/java/io/hops/hopsworks/expat/migrations/conda/DockerConda.java
+++ src/main/java/io/hops/hopsworks/expat/migrations/conda/DockerConda.java
@@ -1,11 +1,7 @@
 package io.hops.hopsworks.expat.migrations.conda;
 
-import io.hops.hopsworks.common.util.ProcessDescriptor;
-import io.hops.hopsworks.common.util.ProcessResult;
 import io.hops.hopsworks.expat.configuration.ConfigurationBuilder;
 import io.hops.hopsworks.expat.configuration.ExpatConf;
-import io.hops.hopsworks.expat.db.DbConnectionFactory;
-import io.hops.hopsworks.expat.executor.ProcessExecutor;
 import io.hops.hopsworks.expat.migrations.MigrateStep;
 import io.hops.hopsworks.expat.migrations.MigrationException;
 import io.hops.hopsworks.expat.migrations.RollbackException;
@@ -13,12 +9,6 @@ import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.concurrent.TimeUnit;
 
 public class DockerConda implements MigrateStep {
   
@@ -42,46 +32,46 @@ public class DockerConda implements MigrateStep {
   @Override
   public void migrate() throws MigrationException {
     //Get all conda enabled projects
-    try (Connection connection = DbConnectionFactory.getConnection();
-         Statement stmt = connection.createStatement();
-         ResultSet resultSet = stmt.executeQuery("SELECT projectname,users.username FROM project " +
-                                                    "JOIN users ON project.username=users.email WHERE conda_env=1;")) {
-      setup();
-      while (resultSet.next()) {
-        String projectName = resultSet.getString("projectname");
-        String username = resultSet.getString("username");
-        //Get username of owner and construct project_user username
-        String projectUser= projectName + "__" + username;
-        
-        try {
-          ProcessDescriptor convaEnvMigrateProc = new ProcessDescriptor.Builder()
-            .addCommand(expatPath + "/bin/conda_env_migrate.sh")
-            .addCommand(projectName)
-            .addCommand(condaDir)
-            .addCommand(condaUser)
-            .addCommand(projectUser)
-            .addCommand(hopsClientUser)
-            .addCommand(hadoopHome)
-            .ignoreOutErrStreams(false)
-            .setWaitTimeout(2, TimeUnit.MINUTES)
-            .build();
-          
-          ProcessResult processResult = ProcessExecutor.getExecutor().execute(convaEnvMigrateProc);
-          if (processResult.getExitCode() == 0) {
-            LOGGER.info("Successfully exported Python env for project: " + projectName);
-          } else {
-            LOGGER.error("Failed to export Python env for project: " + projectName +
-              " " + processResult.getStderr());
-          }
-        } catch (IOException e) {
-          // Keep going
-          LOGGER.error("Failed to export conda env project: " + projectName + " " + e.getMessage());
-        }
-      }
-    } catch (Exception ex) {
-      LOGGER.error(ex.getMessage(), ex);
-      throw new MigrationException("Error in migration step " + DockerConda.class.getSimpleName(), ex);
-    }
+//    try (Connection connection = DbConnectionFactory.getConnection();
+//         Statement stmt = connection.createStatement();
+//         ResultSet resultSet = stmt.executeQuery("SELECT projectname,users.username FROM project " +
+//                                                 "JOIN users ON project.username=users.email WHERE conda_env=1;")) {
+//      setup();
+//      while (resultSet.next()) {
+//        String projectName = resultSet.getString("projectname");
+//        String username = resultSet.getString("username");
+//        //Get username of owner and construct project_user username
+//        String projectUser= projectName + "__" + username;
+//
+//        try {
+//          ProcessDescriptor convaEnvMigrateProc = new ProcessDescriptor.Builder()
+//            .addCommand(expatPath + "/bin/conda_env_migrate.sh")
+//            .addCommand(projectName)
+//            .addCommand(condaDir)
+//            .addCommand(condaUser)
+//            .addCommand(projectUser)
+//            .addCommand(hopsClientUser)
+//            .addCommand(hadoopHome)
+//            .ignoreOutErrStreams(false)
+//            .setWaitTimeout(2, TimeUnit.MINUTES)
+//            .build();
+//
+//          ProcessResult processResult = ProcessExecutor.getExecutor().execute(convaEnvMigrateProc);
+//          if (processResult.getExitCode() == 0) {
+//            LOGGER.info("Successfully exported Python env for project: " + projectName);
+//          } else {
+//            LOGGER.error("Failed to export Python env for project: " + projectName +
+//              " " + processResult.getStderr());
+//          }
+//        } catch (IOException e) {
+//          // Keep going
+//          LOGGER.error("Failed to export conda env project: " + projectName + " " + e.getMessage());
+//        }
+//      }
+//    } catch (Exception ex) {
+//      LOGGER.error(ex.getMessage(), ex);
+//      throw new MigrationException("Error in migration step " + DockerConda.class.getSimpleName(), ex);
+//    }
   }
   
   @Override
